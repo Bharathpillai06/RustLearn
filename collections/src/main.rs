f7cffcd26084c57b06d6b19789d4035328a630ef
Bharathpillai06 <62036034+Bharathpillai06@users.noreply.github.com>
2025-12-04 20:45:25 -0500

    Update main.rs

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -93,6 +93,24 @@ fn main() {
     // field_name and field_value are invalid at this point, try using them and
     // see what compiler error you get!
 
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{scores:?}");
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{map:?}");
+
     
 
 
